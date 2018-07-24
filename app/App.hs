15898c70c9a89b25f92eba0c49f81177363c0f77
Richard Marko <srk@48.io>
2018-07-24 19:58:20 +0200

    move demo to separate file, liveplot consuming stdin PoC

--- app/App.hs
+++ app/App.hs
@@ -1,7 +1,26 @@
 {-# LANGUAGE DataKinds, TypeOperators #-}
 
+import Pipes
+import qualified Pipes.Prelude as P
+import Control.Concurrent (threadDelay)
+
 import Graphics.Liveplot
-import Graphics.Liveplot.Demo
 
 main :: IO ()
-main = runDemo
+main = runLiveplot std
+
+std :: Managed (View (Either (SensorReading GLfloat) GLApp),
+                Controller (Either (SensorReading GLfloat) Event))
+std = do
+  let inits = [ lineGraphN 2000 "adc" (1, 1) (0, 0) ]
+
+  (v, c) <- ogl inits
+
+  dat <- producer (bounded 1) (stdinData >-> normalize (20, 230) >-> named "adc")
+  return (v, fmap Left (dat) <> fmap Right c)
+
+stdinData :: Producer GLfloat IO ()
+stdinData = for P.stdinLn cvt
+  where cvt :: Monad m => String -> Producer Float m ()
+        cvt x = do
+          yield (read x)
